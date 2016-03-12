4918f631c01e5abdf7c86a3e8fae521534265bc7
SAURABH JAIN <saurabhjn76@gmail.com>
2016-03-12 18:36:25 +0530

    Create Link_fetcher.java

---
+++ Java_Files/Link_fetcher.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Link_fetcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner in = new Scanner(System.in);
+		 
+		 System.out.println("Enter the songname\\lyrics\\ artist...");
+		 String s = in.nextLine();
+		 
+		 String query= s.replace(' ', '+');
+		// System.out.println(query);
+		 
+		 
+		 
+
+	}
+
+}
