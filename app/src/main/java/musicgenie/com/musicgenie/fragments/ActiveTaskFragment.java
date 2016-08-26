edbe4b92e091829c32f18d27f2ab6c6712018a91
saurabhjn76 <saurabhjn76@gmail.com>
2016-08-26 19:23:20 +0530

    Limit file size in Mb's.

--- app/src/main/java/musicgenie/com/musicgenie/fragments/ActiveTaskFragment.java
+++ app/src/main/java/musicgenie/com/musicgenie/fragments/ActiveTaskFragment.java
@@ -113,7 +113,7 @@ public class ActiveTaskFragment extends Fragment {
             ArrayList<DownloadTaskModel> old_list = getTasksList();
             for(int i=0;i<old_list.size();i++){
                 if(i==position){
-                    old_list.set(i,new DownloadTaskModel(old_list.get(i).Title,progress,old_list.get(i).taskID,String.valueOf(inMB(contentSize))));
+                    old_list.set(i,new DownloadTaskModel(old_list.get(i).Title,progress,old_list.get(i).taskID,String.valueOf(inMB(contentSize))+"Mb"));
                 }
             }
 
@@ -137,11 +137,20 @@ public class ActiveTaskFragment extends Fragment {
 
         }
     }
+    public static double roundOf(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
 
     private double inMB(String bytes){
         if (bytes != null) {
             double inBytes = Double.parseDouble(bytes);
             double inMB = ((inBytes / 1024) / 1024);
+            inMB=roundOf(inMB,2);
             log("bytes " + bytes);
             
             return inMB;
